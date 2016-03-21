274063b72d7b3a2917fbe4d85e4bd642808b5154
Carlos Allende <carlosallendesanz@gmail.com>
2016-03-21 08:45:40 -0700

    latest changes before weekend

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 console.log ('test')
 var board = $('#board')
+
+
 function boardGenerator(row,column){
   for(i=0; i<row; i++){
-board.append('<div id="row' +i+ '"></div>')
+board.append('<div id="row'+i+'"></div>')
   for(j=0; j<column; j++){
-    $('#row'+i.toString()).append('<button class="tile"></button>')
+    $('#row'+i).append('<div class="column'+j+' tile"></div>')
   }
 }
 }
-boardGenerator(10,20)
+boardGenerator(3,10)
+
+
