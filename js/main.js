5c74e28d2ae8e362df87a88c83c6f7715600712e
Carlos Allende <carlosallendesanz@gmail.com>
2016-03-28 08:36:08 -0700

    js file more efficient

--- js/main.js
+++ js/main.js
@@ -53,12 +53,12 @@ function boardGenerator(rows,columns){
     var randomRow=Math.floor(Math.random()*rows)
     var randomColumn=Math.floor(Math.random()*columns)
     var mine = $('#row'+randomRow).find('.col'+randomColumn)
-     if (mine.hasClass('adjacent')){
+    if (mine.hasClass('adjacent')){
       mine.removeClass('adjacent')
       mine.addClass('mine')
     }
     else {
-    mine.addClass('mine')
+      mine.addClass('mine')
     }
     if ($('#row'+randomRow).find('.col'+(randomColumn-1)).hasClass('mine')==false) {
       $('#row'+randomRow).find('.col'+(randomColumn-1)).addClass('adjacent')
@@ -91,40 +91,29 @@ function boardGenerator(rows,columns){
     var rowNumber = parseInt(row.match(/\d+/)[0],10)
     var column = $(this).attr('class')
     var colNumber = parseInt(column.match(/\d+/)[0],10)
+    var rightTile =$('#row'+rowNumber).find('.col'+(colNumber+1))
+    var leftTile =$('#row'+rowNumber).find('.col'+(colNumber-1))
+    var aboveTile=$('#row'+(rowNumber-1)).find('.col'+colNumber)
+    var aboveLeftTile=$('#row'+(rowNumber-1)).find('.col'+(colNumber-1))
+    var aboveRightTile=$('#row'+(rowNumber-1)).find('.col'+(colNumber+1))
+    var belowTile=$('#row'+(rowNumber+1)).find('.col'+colNumber)
+    var belowLeftTile=$('#row'+(rowNumber+1)).find('.col'+(colNumber-1))
+    var belowRightTile=$('#row'+(rowNumber+1)).find('.col'+(colNumber+1))
+    var neighbors=[rightTile,leftTile,aboveTile,aboveLeftTile,aboveRightTile,belowTile,belowLeftTile,belowRightTile]
     var n = 0
-    if ($('#row'+rowNumber).find('.col'+(colNumber+1)).hasClass('mine')==true){
-      n++
-    }
-    if ($('#row'+rowNumber).find('.col'+(colNumber-1)).hasClass('mine')==true){
-      n++
-    }
-    if ($('#row'+(rowNumber-1)).find('.col'+colNumber).hasClass('mine')==true){
-      n++
-    }
-    if ($('#row'+(rowNumber-1)).find('.col'+(colNumber-1)).hasClass('mine')==true){
-      n++
-    }
-    if ($('#row'+(rowNumber-1)).find('.col'+(colNumber+1)).hasClass('mine')==true){
-      n++
-    }
-    if ($('#row'+(rowNumber+1)).find('.col'+colNumber).hasClass('mine')==true){
-      n++
-    }
-    if ($('#row'+(rowNumber+1)).find('.col'+(colNumber-1)).hasClass('mine')==true){
-      n++
-    }
-    if ($('#row'+(rowNumber+1)).find('.col'+(colNumber+1)).hasClass('mine')==true){
-      n++
-    }
-  $(this).attr('value',''+n+'')
+    neighbors.forEach(function(index,el){
+      if ($(index).hasClass('mine')==true){
+        n++
+      }
+    })
+    $(this).attr('value',''+n+'')
   })
   play()
-
   minesCounter=$('.mine').length
   $('#mines-counter').html(minesCounter)
   $('#restart').click(function() {
-    location.reload();
-});
+    location.reload()
+  })
 }
 
 
@@ -172,88 +161,25 @@ function play (){
           var belowTile=$('#row'+(rowNumber+1)).find('.col'+colNumber)
           var belowLeftTile=$('#row'+(rowNumber+1)).find('.col'+(colNumber-1))
           var belowRightTile=$('#row'+(rowNumber+1)).find('.col'+(colNumber+1))
-          if (rightTile.not('.marked').not('.mine').not('.adjacent').not('.cleared').length > 0) {
-            safeTiles.push(rightTile.not('.marked').not('.mine').not('.adjacent').not('.cleared'))
-          }
-          if (leftTile.not('.marked').not('.mine').not('.adjacent').not('.cleared').length > 0) {
-            safeTiles.push(leftTile.not('.marked').not('.mine').not('.adjacent').not('.cleared'))
-          }
-          if (aboveTile.not('.marked').not('.mine').not('.adjacent').not('.cleared').length > 0) {
-            safeTiles.push(aboveTile.not('.marked').not('.mine').not('.adjacent').not('.cleared'))
-          }
-          if (aboveLeftTile.not('.marked').not('.mine').not('.adjacent').not('.cleared').length > 0) {
-            safeTiles.push(aboveLeftTile.not('.marked').not('.mine').not('.adjacent').not('.cleared'))
-          }
-          if (aboveRightTile.not('.marked').not('.mine').not('.adjacent').not('.cleared').length > 0) {
-            safeTiles.push(aboveRightTile.not('.marked').not('.mine').not('.adjacent').not('.cleared'))
-          }
-          if (belowTile.not('.mine').not('.marked').not('.adjacent').not('.cleared').length > 0) {
-            safeTiles.push(belowTile.not('.marked').not('.marked').not('.mine').not('.adjacent').not('.cleared'))
-          }
-          if (belowLeftTile.not('.mine').not('.adjacent').not('.cleared').length > 0) {
-            safeTiles.push(belowLeftTile.not('.marked').not('.mine').not('.adjacent').not('.cleared'))
-          }
-          if (belowRightTile.not('.mine').not('.marked').not('.adjacent').not('.cleared').length > 0) {
-            safeTiles.push(belowRightTile.not('.marked').not('.mine').not('.adjacent').not('.cleared'))
-          }
+          var neighbors=[rightTile,leftTile,aboveTile,aboveLeftTile,aboveRightTile,belowTile,belowLeftTile,belowRightTile]
+          neighbors.forEach(function(index,el){
+            if($(index).not('.marked').not('.mine').not('.adjacent').not('.cleared').length > 0) {
+              safeTiles.push($(index).not('.marked').not('.mine').not('.adjacent').not('.cleared'))
+            }
+          })
           //3. revealing all unrevealed neighbors
-          if (rightTile.hasClass('mine')==false){
-           rightTile.addClass('cleared')
-          }
-          if (leftTile.hasClass('mine')==false){
-            leftTile.addClass('cleared')
-          }
-          if (aboveTile.hasClass('mine')==false){
-            aboveTile.addClass('cleared')
-          }
-          if (aboveLeftTile.hasClass('mine')==false){
-            aboveLeftTile.addClass('cleared')
-          }
-          if (aboveRightTile.hasClass('mine')==false){
-            aboveRightTile.addClass('cleared')
-          }
-          if (belowTile.hasClass('mine')==false){
-            belowTile.addClass('cleared')
-          }
-          if (belowLeftTile.hasClass('mine')==false){
-            belowLeftTile.addClass('cleared')
-          }
-          if (belowRightTile.hasClass('mine')==false){
-            belowRightTile.addClass('cleared')
-          }
+          neighbors.forEach(function(index,el){
+            if ($(index).hasClass('mine')==false){
+              $(index).addClass('cleared')
+            }
+          })
           //4. revealing the number in adjacent tiles
-          if (rightTile.hasClass('adjacent')==true){
-            value = rightTile.attr('value')
-            rightTile.text(''+value+'')
-          }
-          if (leftTile.hasClass('adjacent')==true){
-            value = leftTile.attr('value')
-            leftTile.text(''+value+'')
-          }
-          if (aboveTile.hasClass('adjacent')==true){
-            value = aboveTile.attr('value')
-            aboveTile.text(''+value+'')
-          }
-          if (aboveLeftTile.hasClass('adjacent')==true){
-            value = aboveLeftTile.attr('value')
-            aboveLeftTile.text(''+value+'')
-          }
-          if (aboveRightTile.hasClass('adjacent')==true){
-            value = aboveRightTile.attr('value')
-            aboveRightTile.text(''+value+'')
-          }
-          if (belowTile.hasClass('adjacent')==true){
-            value = belowTile.attr('value')
-            belowTile.text(''+value+'')
-          }
-          if (belowLeftTile.hasClass('adjacent')==true){
-            value = belowLeftTile.attr('value')
-            belowLeftTile.text(''+value+'')
-          }
-          if (belowRightTile.hasClass('adjacent')==true){
-            value = belowRightTile.attr('value')
-            belowRightTile.text(''+value+'')
-          }
+          neighbors.forEach(function(index,el){
+            if ($(index).hasClass('adjacent')){
+              value=$(index).attr('value')
+              $(index).text(''+value+'')
+            }
+          })
         }
       }
     }
@@ -297,9 +223,10 @@ function mineCounter(){
   $('#mines-counter').text(minesCounter)
 }
 //// win logic
-///if player marks all the mine tiles, wins!!!
+///if player marks all the mine tiles, wins. disable the game and show winning massages.!!!
 function winLogic(){
   allMarked=true
+  console.log($('.mine'))
   $('.mine').each(function(index,el){
     if (!$(el).hasClass('marked')){
       allMarked=false
